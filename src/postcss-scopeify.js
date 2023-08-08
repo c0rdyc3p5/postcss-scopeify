3b165e8369b687a2196e9119ddce321011dcfbbb
c0rdyc3p5 <kevinlecado@gmail.com>
2023-08-08 11:51:22 -0400

    Add test for commas Fix rules when there are commas Update version

--- src/postcss-scopeify.js
+++ src/postcss-scopeify.js
@@ -12,10 +12,23 @@ module.exports = (opts = {}) => {
         postcssPlugin: 'postcss-scopeify',
         Root(root) {
             root.walkRules((rule) => {
-                const selector = rule.selector;
-                if (exclude.some((e) => e.trim() === selector.trim())) {
+                const selector = rule.selector.replace(/\n/g, '');
+
+                // If there are commas, we need to split the selector and scope each one
+                if (selector.includes(',')) {
+                    const selectors = selector.split(',');
+                    const scopedSelectors = selectors.map((s) => {
+                        if (exclude.some((e) => e.trim() === s.trim())) {
+                            return s;
+                        }
+                        return opts.scope + s;
+                    });
+                    rule.selector = scopedSelectors.join(',');
+                    return;
+                } else if (exclude.some((e) => e.trim() === selector.trim())) {
                     return;
                 }
+
                 const prefix = opts.scope;
                 rule.selector = prefix + selector;
             });
